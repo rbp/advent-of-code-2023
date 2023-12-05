6761247072cd70e39468e675d6f83202412e94cf
Rodrigo Bernardo Pimentel <rbp@isnomore.net>
2023-12-05 20:58:29 +0100

    Solve day 4, part 2

--- day4/scratchcard.go
+++ day4/scratchcard.go
@@ -11,7 +11,7 @@ var colon *regexp.Regexp = regexp.MustCompile(`\s*:\s*`)
 var pipe *regexp.Regexp = regexp.MustCompile(`\s*\|\s*`)
 var ws *regexp.Regexp = regexp.MustCompile(`\s+`)
 
-func cardValue(line string) int {
+func parseLine(line string) (map[string]bool, []string) {
 	parts := colon.Split(line, 2)
 	numbers := pipe.Split(parts[1], 2)
 	winning := ws.Split(numbers[0], -1)
@@ -21,24 +21,55 @@ func cardValue(line string) int {
 	for _, w := range winning {
 		winningSet[w] = true
 	}
+	return winningSet, won
+}
 
+func countWon(winningSet map[string]bool, won []string) int {
 	value := 0
 	for _, w := range won {
 		if winningSet[w] {
-			if value == 0 {
-				value = 1
-			} else {
-				value <<= 1
-			}
+			value++
 		}
 	}
 	return value
 }
 
+func cardValue(line string) int {
+	winningSet, won := parseLine(line)
+	nWon := countWon(winningSet, won)
+	if nWon <= 1 {
+		return nWon
+	}
+	return 1 << (nWon - 1)
+}
+
+func collecCards(lines []string) int {
+	nCards := make([]int, len(lines))
+
+	for i, line := range lines {
+		// By definition, we always have at least one copy of the cards we're looking at
+		nCards[i]++
+		winningSet, won := parseLine(line)
+		nWon := countWon(winningSet, won)
+		for j := 0; j < nWon && j < len(lines); j++ {
+			nCards[i+j+1] += nCards[i]
+		}
+	}
+	sum := 0
+	for _, n := range nCards {
+		sum += n
+	}
+	return sum
+}
+
 func main() {
+	lines := advent.Readlines(os.Args[1])
 	total := 0
-	for _, line := range advent.Readlines(os.Args[1]) {
+	for _, line := range lines {
 		total += cardValue(line)
 	}
 	fmt.Printf("Part 1: %d\n", total)
+
+	nCards := collecCards(lines)
+	fmt.Printf("Part 2: %d\n", nCards)
 }
